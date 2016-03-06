c6fe3348948c79c1c3db789c2dfeceea456d6fb6
Vicky Kao <qvicky@gmail.com>
2016-03-06 11:10:27 +0800

    FabricsEntities 的 SaveChanges() 進行例外處裡。

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using System.Data.Entity.Validation;
 
 namespace MVC5Course.Controllers
 {
@@ -16,17 +17,47 @@ namespace MVC5Course.Controllers
 
             db.Product.Add(new Product() {
                 ProductName = "test123",
-                Price = 5,
+                Price = 3,
                 Stock = 1,
                 Active = true
             });
-             db.SaveChanges();
+
+            try {
+                db.SaveChanges();
+            }
+            catch (DbEntityValidationException ex) {
+                foreach (DbEntityValidationResult item in ex.EntityValidationErrors) {
+                    string entityName = item.Entry.Entity.GetType().Name;
+
+                    foreach (DbValidationError err in item.ValidationErrors) {
+                        throw new Exception(entityName + " 類型驗證失敗 : " + err.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            catch (Exception ex) {
+                
+                throw;
+            }
 
              var data = db.Product.ToList();
 
             return View(data);
         }
+        public ActionResult CustomerOrder() {
+            var db = new FabricsEntities();
+            var query = (from o in db.Order
+                         join c in db.Client on o.ClientId equals c.ClientId
+                         select new {
+                             c.FirstName,
+                             c.LastName,
+                             o.OrderDate,
+                             o.OrderTotal,
+                             o.OrderStatus
+                         });
 
+            return View(query);
+        }
 
     }
 }
