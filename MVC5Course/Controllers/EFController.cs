bca27c547ed430bc28d3fd7a861c64cf1ae484ff
Vicky Kao <qvicky@gmail.com>
2016-03-06 10:32:32 +0800

    FabricsEntities 物件服務新增一筆 Product 資料，並顯示在頁面上

---
+++ MVC5Course/Controllers/EFController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class EFController : Controller
+    {
+        // GET: EF
+        public ActionResult Index()
+        {
+            var db = new FabricsEntities();
+
+            db.Product.Add(new Product() {
+                ProductName = "test123",
+                Price = 5,
+                Stock = 1,
+                Active = true
+            });
+             db.SaveChanges();
+
+             var data = db.Product.ToList();
+
+            return View(data);
+        }
+
+
+    }
+}
