d6552cd78c66daf896f0a70f66aac6eca9913a82
Vicky Kao <qvicky@gmail.com>
2016-03-19 14:35:14 +0800

    將 /Client/Index 頁面 Ajax 化的 Edit 功能改成 Ajax.BeginForm 版本

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -88,7 +88,9 @@ namespace MVC5Course.Controllers
             {
                 db.Entry(client).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+
+                return View("Index", db.Client.Include(c => c.Occupation).Take(5));
+                //return RedirectToAction("Index");
             }
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
             return View(client);
